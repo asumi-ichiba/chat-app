d53adb6eff6dd81a8eec88286c2121eb2f4148b7
asumi-ichiba <121480112+asumi-ichiba@users.noreply.github.com>
2023-01-24 16:53:37 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -11,6 +12,7 @@ class MessagesController < ApplicationController
       redirect_to room_messages_path(@room)
     else
       render :index
+      @messages = @room.messsages.inckudes(:user)
     end
   end
 
